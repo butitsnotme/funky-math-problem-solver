cdc5d7b1f3bfcd096a62603fe5e4e8964ad50ef8
Dennis Bellinger <dennis.bellinger@gmail.com>
2016-10-06 13:31:07 -0400

    Added a couple of metrics about solutions

--- inc/Solver.h
+++ inc/Solver.h
@@ -2,6 +2,8 @@
 #define SOLVER_H_INCLUDED
 
 #include <vector>
+#include <chrono>
+#include <ctime>
 
 #include "Rule.h"
 
@@ -10,17 +12,23 @@ public:
   Solver(int size, std::vector<Rule> ruleset);
   ~Solver();
   void solve();
-  void print() const;
+  void print();
   void findAll(bool all);
   void countOnly(bool count);
+  void enableMetrics(bool metrics);
 private:
   bool step(int x, int y);
   bool all = false;
   bool onlyCount = false;
+  bool metrics = false;
   int size;
+  int backtracks = 0;
+  int backtracks_total = 0;
   std::vector<Rule> ruleset;
   std::vector<std::vector<int>> matrix;
   int found = 0;
+  std::chrono::steady_clock::time_point start;
+  std::chrono::steady_clock::time_point start_total;
 };
 
 #endif //SOLVER_H_INCLUDED
